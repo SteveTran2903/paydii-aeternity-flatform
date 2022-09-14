79569f586ad73c1bef2085853a8e62c768483342
Deakil <Namth3979@gmail.com>
2022-09-14 17:22:06 +0700

    coding

--- components/IndexComponents/BuyProductButton.tsx
+++ components/IndexComponents/BuyProductButton.tsx
@@ -54,7 +54,7 @@ export default function BuyProductButton(props: any) {
     e.preventDefault()
     console.log(`Buy product with ${idProduct} `)
     if (validCoupon) {
-      // buyProductWithCoupon()
+      buyProductWithCoupon()
     } else {
       buyProduct()
     }
@@ -78,123 +78,41 @@ export default function BuyProductButton(props: any) {
   //     // return response.json(); // parses JSON response into native JavaScript objects
   // }
 
-    const buyProduct = async () => {
+  const buyProduct = async () => {
 
-      console.log("buyProduct")
+    console.log("buyProduct")
 
-      let buyProduct = await dataUserSession.contractInstance.methods.buy_product(idProduct,false, '', {amount : priceProduct*(10**18)})
+    let buyProduct = await dataUserSession.contractInstance.methods.buy_product(idProduct, false, '', { amount: priceProduct * (10 ** 18) })
 
-      console.log(buyProduct)
+    console.log(buyProduct)
 
-      // let tx = await this.contractInstance.transfer('ak_qTraJXXjNMG3or2pNsN6rgemqm9Sjpie68xuUjnwnNS8Vih1s', 1000000000000000000 , {amount : 1000000000000000000})
+    Swal.fire({
+      icon: 'success',
+      title: 'Buying products on blockchain, please wait a moment!',
+      text: 'Estimated completion time: 3 to 5 minutes or maybe sooner',
+      showConfirmButton: true
+    }).then((result) => {
+      // router.push('/seller/catalog/products')
+    })
+  }
 
+  const buyProductWithCoupon = async () => {
 
+    console.log("buyProduct With coupon")
+    
+    let buyProduct = await dataUserSession.contractInstance.methods.buy_product(idProduct, true, couponCode,{ amount: priceProduct * (10 ** 18) })
 
-      // const network = new currentNetwork()
+    console.log('buyProduct withcoupon',buyProduct)
 
-      // const priceProductConverted =  uintCV(priceProduct)
-
-      // const numberStxWillSendToSeller = priceProductConverted.value - (uintCV(basisPoint).value * priceProductConverted.value)/ uintCV(10000).value
-
-      // console.log(priceProductConverted.value, numberStxWillSendToSeller)
-
-      // const standardStxPostCondition = makeStandardSTXPostCondition(
-      //   addressBuyer,
-      //   FungibleConditionCode.Equal,
-      //   priceProductConverted.value
-      // )
-
-      // const contractStxPostCondition = makeContractSTXPostCondition(
-      //   contractOwnerAddress,
-      //   contractName,
-      //   FungibleConditionCode.Equal,
-      //   numberStxWillSendToSeller,
-      // )
-
-      // // (contract-call? .paydii create-product name img price isactive)
-      // const options: ContractCallRegularOptions = {
-      //   contractAddress: contractOwnerAddress,
-      //   contractName: contractName,
-      //   functionName: 'buy-product',
-      //   functionArgs:  [
-      //     stringAsciiCV(idProduct)
-      //   ],
-      //   postConditions: [standardStxPostCondition,contractStxPostCondition],
-      //   network,
-      //   appDetails,
-      //   onFinish: ({ txId }) => {
-      //     writeProductIdAndTxIdToJsonFile(addressBuyer, txId, 'buy-product')
-      //     addTransactionToast(txId, `Buy Product ${idProduct} to ${truncateMiddle(contractOwnerAddress)}...`)
-      //     Swal.fire({
-      //       icon: 'success',
-      //       title: 'Buying products on blockchain, please wait a moment!',
-      //       text: 'Estimated completion time: 3 to 5 minutes or maybe sooner',
-      //       showConfirmButton: true
-      //     }).then((result) => {
-      //       // router.push('/seller/catalog/products')
-      //     })
-
-      //   },
-      // }
-      // await openContractCall(options)
-    }
-
-  //   const buyProductWithCoupon = async () => {
-
-  //     const network = new currentNetwork()
-
-  //     const priceProductConverted =  uintCV(priceProduct)
-
-  //     let amountDiscountFinal:any = null
-  //     if(isDiscountPercent) {
-  //       amountDiscountFinal = (uintCV(discountAmountValue).value * priceProductConverted.value)/ uintCV(10000).value
-  //     } else {
-  //       amountDiscountFinal = uintCV(discountAmountValue).value
-  //     } 
-
-  //     let newPriceAfterDiscount = priceProductConverted.value - amountDiscountFinal
-
-  //     if(newPriceAfterDiscount <0) {
-  //       newPriceAfterDiscount = uintCV(0).value
-  //     }
-
-  //     const numberStxWillSendToSeller = newPriceAfterDiscount - (uintCV(basisPoint).value * newPriceAfterDiscount)/ uintCV(10000).value
-
-  //     console.log(newPriceAfterDiscount, numberStxWillSendToSeller)
-
-  //     const standardStxPostCondition = makeStandardSTXPostCondition(
-  //       addressBuyer,
-  //       FungibleConditionCode.Equal,
-  //       newPriceAfterDiscount
-  //     )
-
-  //     const contractStxPostCondition = makeContractSTXPostCondition(
-  //       contractOwnerAddress,
-  //       contractName,
-  //       FungibleConditionCode.Equal,
-  //       numberStxWillSendToSeller,
-  //     )
-
-  //     // (contract-call? .paydii create-product name img price isactive)
-  //     const options: ContractCallRegularOptions = {
-  //       contractAddress: contractOwnerAddress,
-  //       contractName: contractName,
-  //       functionName: 'buy-product-with-coupon',
-  //       functionArgs:  [
-  //         stringAsciiCV(idProduct),
-  //         stringAsciiCV(couponCode)
-  //       ],
-  //       postConditions: [standardStxPostCondition,contractStxPostCondition],
-  //       network,
-  //       appDetails,
-  //       onFinish: ({ txId }) => {
-  //         console.log('buy product success',txId)
-  //         addTransactionToast(txId, `Buy Product ${idProduct} to ${truncateMiddle(contractOwnerAddress)}...`)
-  //         // alert('you buy this product successfully')
-  //       },
-  //     }
-  //     await openContractCall(options)
-  //   }
+    Swal.fire({
+      icon: 'success',
+      title: 'Buying products on blockchain, please wait a moment!',
+      text: 'Estimated completion time: 3 to 5 minutes or maybe sooner',
+      showConfirmButton: true
+    }).then((result) => {
+      // router.push('/seller/catalog/products')
+    })
+  }
 
   const renderTextValidCoupon = () => {
 
@@ -206,8 +124,8 @@ export default function BuyProductButton(props: any) {
         return (<div className="coupon-alert-text">Coupon is not valid </div>)
       }
       else {
-        let formatPrice = priceProduct / 10**18
-        let discountAmountValueFormat = discountAmountValue / 10**18
+        let formatPrice = priceProduct * 10 ** 18
+        let discountAmountValueFormat = discountAmountValue
         let newPriceTypeAmount = formatPrice - discountAmountValueFormat
 
         console.log('discount amount value', newPriceTypeAmount)
@@ -215,7 +133,7 @@ export default function BuyProductButton(props: any) {
           return (
             <div className="coupon-success-text mt-2">
               <div>
-                Your Coupon is {discountAmountValueFormat} STX off
+                Your Coupon is {discountAmountValueFormat/(10**18)} STX off
               </div>
               <div className='text-danger'>
                 Please contact the seller to get this product for free!
@@ -226,10 +144,10 @@ export default function BuyProductButton(props: any) {
           return (
             <div className="coupon-success-text mt-2">
               <div>
-                Your Coupon is {discountAmountValueFormat} STX off
+                Your Coupon is {discountAmountValueFormat/10**18} STX off
               </div>
               <div>
-                The current price of the product is <span className="new-price-discount">{newPriceTypeAmount} STX</span>
+                The current price of the product is <span className="new-price-discount">{newPriceTypeAmount/10**18} STX</span>
               </div>
             </div>
           )
@@ -245,10 +163,10 @@ export default function BuyProductButton(props: any) {
     setDiscountAmountValue(discountAmount)
     setIsDiscountPercent(false)
 
-    let formatPrice = priceProduct / 10**18
-    let discountAmountValueFormat = discountAmount /  10**18
+    let formatPrice = priceProduct * 10 ** 18
+    let discountAmountValueFormat = discountAmount
     let newPriceTypeAmount = formatPrice - discountAmountValueFormat
-    
+
 
     if (newPriceTypeAmount <= 0) {
       setDisableBuyButton(true)
