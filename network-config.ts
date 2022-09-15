868b34e5a09693472110dd0a100a980840e8cab7
Deakil <Namth3979@gmail.com>
2022-09-15 23:02:06 +0700

    coding

--- network-config.ts
+++ network-config.ts
@@ -1,6 +1,6 @@
 
 // For testnet
-let contractAddress = 'ct_TzEJJgUaCX9FgeQnwRMBco5m9TGU7CBVBURtAjf9ntSAYJboT'
+let contractAddress = 'ct_uTnwKXLNn3jhzVFFCxcC9iiWCXCtJNJj9YriVnKvx7S1Fy5nG'
 let urlCallAPI = 'https://testnet.aeternity.io'
 let networkName = 'testnet'
 
@@ -10,7 +10,7 @@ console.log('network-config')
 if (typeof window !== 'undefined') {
     if (localStorage.getItem('NETWORK_AETERNITY') == 'mainnet') {
         // For mainnet
-        let contractAddress = 'ct_TzEJJgUaCX9FgeQnwRMBco5m9TGU7CBVBURtAjf9ntSAYJboT'
+        let contractAddress = 'ct_uTnwKXLNn3jhzVFFCxcC9iiWCXCtJNJj9YriVnKvx7S1Fy5nG'
         const urlCallAPI = 'https://mainnet.aeternity.io'
         let networkName = 'mainnet'
     }
